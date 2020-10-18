018397b8e91e91d117d965eaa4e02c74906ff321
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-18 12:42:07 -0400

    updated files to work on heroku

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const router = require('express').Router();
-let { notes } = require("../data/db");
-const {addNote, validateNote, deleteById} = require("../lib/notes");
+let { notes } = require("../data/db.json");
+const {addNote, validateNote} = require("../lib/notes.js");
 const shortid = require('shortid');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 router.get("/api/notes", (req, res) => {
     res.json(notes);
-  });
+});
 
-  router.post("/api/notes", (req, res) => {
+router.post("/api/notes", (req, res) => {
     req.body.id = shortid.generate();
     if (!validateNote(req.body)) {
-      res.status(400).send("You must enter a note in all text-inputs");
+        res.status(400).send("You must enter a note in all text-inputs");
     } else {
-      const note = addNote(req.body, notes);
-      res.json(note);
+        const note = addNote(req.body, notes);
+        res.json(note);
     }
-  });
+});
 
 module.exports = router;
