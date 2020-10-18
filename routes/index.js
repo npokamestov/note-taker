97012d0924639ae20aaa66be6d351bbae9cec536
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-18 11:52:51 -0400

    added server.js and index.js for routes

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const router = require('express').Router();
+let { notes } = require("../data/db");
+const {addNote, validateNote, deleteById} = require("../lib/notes");
+const shortid = require('shortid');
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get("/api/notes", (req, res) => {
+    res.json(notes);
+  });
+
+  router.post("/api/notes", (req, res) => {
+    req.body.id = shortid.generate();
+    if (!validateNote(req.body)) {
+      res.status(400).send("You must enter a note in all text-inputs");
+    } else {
+      const note = addNote(req.body, notes);
+      res.json(note);
+    }
+  });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const routes = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', routes);
+
+app.use(express.static('public'));   
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
